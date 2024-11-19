8a6c8225f1a701115e0c8886d4dcd69b2efb4164
Hiran S <hirans2006@gmail.com>
2024-11-19 18:52:55 +0530

    Notes Update

--- NoteBook/index.js
+++ NoteBook/index.js
@@ -1,16 +1,22 @@
 const button=document.getElementById("addBtn");
 const main=document.querySelector(".main");
 button.addEventListener("click",addNote);
+LoadPage();
+function LoadPage(){
+    const data = JSON.parse(localStorage.getItem("note"));
+    data.forEach((d)=>addNote(d));
+}
 
-function addNote(){
+function addNote(text = ""){
     const node = document.createElement("div");
     node.classList.add("note");
+    console.log(text);
     node.innerHTML=`
             <div class="tool">
                 <img src="Save_icon.png" alt="" class="save">
                 <img src="trash-bin.png" alt="" class="trash">
             </div>
-            <textarea></textarea>
+            <textarea>${text}</textarea>
     `;
     node.querySelector(".trash").addEventListener("click",()=>{
         node.remove();
